4ba2defe637f1dcd214f0108665be195bb7fc24b
Eddy Snow <eddysnow@gmail.com>
2019-02-26 17:19:28 +0200

    fix delete script

--- Deploy/k8s-delete.sh
+++ Deploy/k8s-delete.sh
@@ -7,4 +7,4 @@ K8S_IAM_NAME="eddy-k8s-iam"
 export AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name $K8S_IAM_NAME --region $AWS_REGION --profile $AWS_PROFILE | jq --raw-output '.Stacks[].Outputs[] | select(.OutputKey=="AccessKeyId").OutputValue')
 export AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name $K8S_IAM_NAME --region $AWS_REGION --profile $AWS_PROFILE | jq --raw-output '.Stacks[].Outputs[] | select(.OutputKey=="SecretAccessKey").OutputValue')
 
-kops delete cluster --name eddy.eu.sbx.kube.intapp.com --state="s3://k8s-clusterstatestorage-eddy-k8s-environment-eu-west-1" --yes
+kops delete cluster --name eddy.eu.sbx.kube.intapp.com --state="s3://k8s-clusterstatestorage-eddy-k8s-state-us-east-1" --yes
