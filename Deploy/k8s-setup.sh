a24d075d09b3a63bf66f817c45162d6b1786dc89
Eddy Snow <eddysnow@gmail.com>
2019-03-25 23:49:36 +0000

    deploy wordpress and sort out namespaces

--- Deploy/k8s-setup.sh
+++ Deploy/k8s-setup.sh
@@ -11,28 +11,31 @@
 # Dashboard password is: Password01 (it can be generated using htpasswd
 
 echo "[$(date)] - Applying namespace"
-kubectl apply -f ../Namespaces/default.yaml
+kubectl apply -f ../Namespaces/ops.yaml
 
 echo "[$(date)] - Setting up helm"
-kubectl apply -f ../Authorization/helm/helm_default.yaml
+kubectl apply -f ../Authorization/helm/helm_ops.yaml
 
 echo "[$(date)] - Installing tiller"
-helm init --service-account tiller --tiller-namespace=default
+helm init --service-account tiller --tiller-namespace=ops
 
 echo "[$(date)] - Setting up nginx ingress controller"
-helm upgrade -i -f ../Helm/ingress-nginx/values.yaml --wait --timeout 300 ingress-nginx stable/nginx-ingress --tiller-namespace=default
+helm upgrade -i -f ../Helm/ingress-nginx/values.yaml --wait --timeout 60 ingress-nginx stable/nginx-ingress --tiller-namespace=ops --namespace=ops
 
 # !!!NOTE!!!
 # Need to add AWS alias for each service to the ELB created in the above deployment
 
+echo "[$(date)] - Install wordpress"
+helm upgrade -i -f ../Helm/wordpress/values.yaml --wait --timeout 120 wordpress stable/wordpress --tiller-namespace=ops --namespace=ops
+
 echo "[$(date)] - Install grafana"
-helm upgrade -i -f ../Helm/grafana/values.yaml --wait --timeout 600 grafana stable/grafana --tiller-namespace=default
+helm upgrade -i -f ../Helm/grafana/values.yaml --wait --timeout 60 grafana stable/grafana --tiller-namespace=ops --namespace=ops
 
 echo "[$(date)] - Install prometheus"
-helm upgrade -i -f ../Helm/prometheus/values.yaml --wait --timeout 600 prometheus stable/prometheus --tiller-namespace=default
+helm upgrade -i -f ../Helm/prometheus/values.yaml --wait --timeout 120 prometheus stable/prometheus --tiller-namespace=ops --namespace=ops
 
 echo "[$(date)] - Install jenkins"
-helm upgrade -i -f ../Helm/jenkins/values.yaml --wait --timeout 600 jenkins stable/jenkins --tiller-namespace=default
+helm upgrade -i -f ../Helm/jenkins/values.yaml --wait --timeout 400 jenkins stable/jenkins --tiller-namespace=ops --namespace=ops
 
 #simple-site
 #helm upgrade simple-site ../Helm/simple-site --namespace default --tiller-namespace=default --install
