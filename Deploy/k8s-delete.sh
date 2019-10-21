3b6ce9a76ff0e67a8428c5d5a80d21d818f669f0
esnow <edward.snow@disneystreaming.com>
2019-10-21 15:22:42 +0100

    update delete cluster script to use same variables as deploy

--- Deploy/k8s-delete.sh
+++ Deploy/k8s-delete.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
 AWS_PROFILE="snowco"
-AWS_REGION="eu-west-1"
-K8S_IAM_NAME="k8s-lab-iam"
-CLUSTER_STATE_BUCKET="722777194664-k8s-clst-state-k8s-lab-env-eu-west-1"
+AWS_REGION="eu-west-2"
+NAME="lab"
+K8S_STACK_NAME="k8s-lab"
+K8S_DNS_DOMAIN="kube.esnow.uk"
+K8S_IAM_NAME="$K8S_STACK_NAME-iam"
+K8S_ENV_NAME="$K8S_STACK_NAME-env"
+CLUSTER_NAME="$NAME.$K8S_DNS_DOMAIN"
+K8S_NETWORK="10.10"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text --profile $AWS_PROFILE)
+CLUSTER_STATE_BUCKET="$AWS_ACCOUNT_ID-k8s-clst-state-$K8S_ENV_NAME-$AWS_REGION"
 
 export AWS_ACCESS_KEY_ID=$(aws cloudformation describe-stacks --stack-name $K8S_IAM_NAME --region $AWS_REGION --profile $AWS_PROFILE | jq --raw-output '.Stacks[].Outputs[] | select(.OutputKey=="AccessKeyId").OutputValue')
 export AWS_SECRET_ACCESS_KEY=$(aws cloudformation describe-stacks --stack-name $K8S_IAM_NAME --region $AWS_REGION --profile $AWS_PROFILE | jq --raw-output '.Stacks[].Outputs[] | select(.OutputKey=="SecretAccessKey").OutputValue')
 
-kops delete cluster --name lab.kube.esnow.uk --state="s3://"$CLUSTER_STATE_BUCKET --yes
+kops delete cluster --name $CLUSTER_NAME --state="s3://"$CLUSTER_STATE_BUCKET --yes
